fe14b91421f7bb4125e18df842f8d4c0d6cb0834
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-19 16:39:52 +0530

    Add files via upload

---
+++ day9/functionality/Interface1.cs
@@ -0,0 +1,19 @@
+﻿ ﻿using Day_9.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace Day_9.Functionality
+    {
+        public interface IFootballListMethod
+        {
+            int CreatePlayer(Player player);
+
+            List<Player> GetPlayers();
+            void UpdatePlayer(int id);
+            void SearchPlayer(int id);
+            void RemovePlayer(int id);
+        }
+    }
+
+
