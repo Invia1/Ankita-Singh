99cd9944f2202a25b450662301840f109c2c5193
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-19 09:17:06 +0530

    Add files via upload

---
+++ dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GFG
+{
+    static public void Main()
+    {
+
+        Dictionary<int, string> My_dict1 = new Dictionary<int, string>();
+
+        My_dict1.Add(1, "Welcome");
+        My_dict1.Add(2, "in");
+        My_dict1.Add(3, "Development");
+
+        foreach (KeyValuePair<int, string> ele1 in My_dict1)
+        {
+            Console.WriteLine("{0} and {1}",
+                        ele1.Key, ele1.Value);
+        }
+        Console.WriteLine();
+
+        Dictionary<string, string> My_dict2 = new Dictionary<string, string>()
+        {
+                                {"a.1", "dotnet"},
+                                {"a.2", "sql"},
+                                {"a.3", "..."}
+        };
+
+        foreach (KeyValuePair<string, string> ele2 in My_dict2)
+        {
+            Console.WriteLine("{0} and {1}", ele2.Key, ele2.Value);
+        }
+    }
+}
