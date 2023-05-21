d9fab4c5354131da345c8e6d7c91e64b0c13cdef
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-21 17:45:21 +0530

    Add files via upload
    PRACTICE

---
+++ footballapp/model/main/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using footballapp.services;
+
+namespace footballapp.model.main
+{
+    public class Footballmain
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ footballapp/services/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using footballapp.services;
+using footballapp.model;
+
+namespace footballapp.services
+{
+    public class fbservice
+    {
+
+        List<object> ls = new List<object>();
+        public void add()
+        {
+            ls.Add(new object());
+        }
+
+        //Class1 obj=new Class1 obj();
+
+
+    }
+
+}
