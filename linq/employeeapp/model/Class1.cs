8668676555a468cefa415910b7b36c78e84e2adf
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 16:32:42 +0530

    Add files via upload
    using LINQ in employee App

---
+++ linq/employeeapp/model/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace employeeapp.model
+{
+    public class employee
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public int Eage { get; set; }
+
+
+    }
+}
---
+++ linq/employeeapp/ui/MainApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using employeeapp.model;
+using employeeapp.service;
+using employeeapp.Interface1;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+
+namespace employeeapp.ui
+{
+   public class entry
+    {
+        public static void Main()
+        {
+           List<employee> ls = new List<employee>();
+           employee obj = new employee();
+
+          //empservice obj= new empservice();
+          obj.EId = 100;
+            obj.Eage = 22;
+            obj.EName = "ankita";
+         
+         
+
+
+         EmployeeInterface EI= new empservice();  //encapsulation
+            EI.createEmployee(obj);
+          
+
+            employee obj1 = new employee();
+
+            obj1.EId = 101;
+            obj1.Eage = 22;
+            obj1.EName = "sona";
+            EI.createEmployee(obj1);
+          
+           // EI.createEmployee(obj1);
+
+            List<employee> obj1list = EI.GetEmployeelist();
+
+            
+
+            foreach (employee item in obj1list)
+            {
+                Console.WriteLine(item.EId+" "+item.Eage+" "+item.EName);
+            }
+
+            //linq .......
+
+            var employees = obj1list.Where(employee => employee.EName== "sona");
+
+         //   Console.WriteLine(employees.Ename);
+            foreach (employee item in employees)
+            {
+                Console.WriteLine(item.EName); 
+            }
+
+          
+
+
+        }
+    }
+}
