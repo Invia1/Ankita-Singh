0978c0f92e2f0cf62af109717610c8592f40c0a1
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 21:36:33 +0530

    Add files via upload

---
+++ inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class inherit
+{
+    public void method()
+    {
+        Console.WriteLine("method1");
+    }
+    public void method2() 
+    {
+        Console.WriteLine("method2");
+    }
+
+    class child:inherit
+    {
+       public void method3()
+        {
+            Console.WriteLine("Method3");
+        }
+        public static void Main()
+        {
+           
+           child obj = new child();
+            obj.method();
+            obj.method2();
+            obj.method3();
+            
+        }
+    }
+}
+
