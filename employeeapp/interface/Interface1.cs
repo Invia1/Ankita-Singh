43bf7483706abc0ccc44860421c3f84f876995d5
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 13:56:28 +0530

    Add files via upload

---
+++ employeeapp/interface/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using employeeapp.model;
+using System;
+
+
+namespace employeeapp.Interface1
+{
+   public interface EmployeeInterface
+    { 
+        int createEmployee(employee emp);
+
+        List<employee> GetEmployeelist();
+    }
+}
---
+++ employeeapp/service/Service.cs
@@ -0,0 +1,28 @@
+﻿using employeeapp.model;
+using System;
+using employeeapp.Interface1;
+using System.Collections.Generic;
+
+namespace employeeapp.service
+{
+    public class empservice : EmployeeInterface
+       
+    {
+        //EmployeeInterface EI = new empservice();
+
+        List<employee> ls = new List<employee>();    
+        public int createEmployee(employee emp)
+        {
+            ls.Add(emp);
+            return 1;
+        }
+
+      public List<employee> GetEmployeelist()
+        {
+           
+            return ls;
+        }
+      
+
+    }
+}
