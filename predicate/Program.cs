aaae18a683074a8880a32b02b22d53f9cb8865ff
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 16:38:09 +0530

    Add files via upload

---
+++ predicate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class demo
+{
+    public static bool myfun(string mystring)
+    {
+
+        if (mystring.Length<=17)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        };
+
+
+    }
+
+    public static void Main()
+    {
+        Predicate<string> val = myfun;
+        Console.WriteLine(val("C# coding @1223 "));
+        Console.WriteLine(val("hey coding world..."));
+    }
+}
