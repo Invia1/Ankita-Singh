9445007eef90895e975dc21d4db409b54b09a647
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 14:30:41 +0530

    Add files via upload

---
+++ func delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace System;
+
+class calculate
+{
+    public static int myfun(int a,double b, double c, int d)
+    {
+        return Convert.ToInt32(a * b *c * d);
+    }
+
+   public static void Main()
+    {
+        Func<int, double, double, int ,int > myfunc = myfun;
+        Console.WriteLine(myfun(10,10.5,1.5,1));
+    }
+}
