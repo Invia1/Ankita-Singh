b003e1f49c1e4a4e8346790a9eff7b067249ef6e
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 15:22:28 +0530

    Add files via upload

---
+++ action delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class math
+{
+    public static void calculate(int a ,int b,int c,int d)
+    {
+        Console.WriteLine(a+b+c+d);
+        Console.WriteLine(a+b-c+d);
+        Console.WriteLine(a * b * c);
+        Console.WriteLine((a + b+c+d)/4);
+    }
+
+    public static void Main()
+    {
+        Action<int, int, int, int> val = calculate;
+        
+        val(10, 20,10,30);
+    }
+}
