c74d2e2e3d3eadf4e24087be6c3beeb2e4ba4602
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 11:37:10 +0530

    Add files via upload

---
+++ day15_delegate/Program.cs
@@ -0,0 +1,60 @@
+﻿
+delegate double delexpense();
+delegate int delstudy();
+delegate string delhobby();
+delegate string delsubject();
+delegate void delplay(string n);
+class person
+{
+   public double expense()
+    {
+        return 200.75;
+    }
+
+    public int study()
+    {
+        return 2;
+    }
+     
+    public string hobby()
+    {
+        return "coding";
+    }
+    public string subject()
+    {
+        return "oops";
+    }
+
+    public void address(string n)
+    {
+        Console.WriteLine("noida");
+    }
+
+} 
+
+class friend
+{
+    public static void Main()
+    {
+       person obj= new person();
+
+        delexpense mydel = new delexpense(obj.expense);
+        var temp = mydel();
+        Console.WriteLine(temp);
+
+        delstudy mydele= new delstudy(obj.study);
+        var temp2 = mydele();
+        Console.WriteLine(temp2);
+
+        delhobby mydel1 =new delhobby(obj.hobby);
+        var temp3 = mydel1();
+        Console.WriteLine(temp3);
+
+        //delplay mydel2 = new delplay(string n);
+        delplay obj2 = new delplay(obj.address);
+        obj2(" ");
+            
+
+
+    }
+}
